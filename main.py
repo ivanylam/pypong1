33ee3fefc45a435bf6fdf439d1afd15bd0ff4a02
Ivan.Lam <ivan.lam@fglsports.com>
2022-03-07 14:47:08 -0700

    Scoreboard added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from  scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(800, 600)
@@ -9,20 +10,21 @@ screen.bgcolor("black")
 screen.title("PyPong")
 screen.tracer(0)
 
-r_paddle = Paddle((-350, 0))
-l_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
 ball = Ball()
 screen.listen()
+scoreboard = Scoreboard()
 
-screen.onkey(l_paddle.go_up(), "Up")
-screen.onkeypress(l_paddle.go_up, "Up")
-screen.onkey(l_paddle.go_down, "Down")
-screen.onkeypress(l_paddle.go_down, "Down")
+screen.onkey(r_paddle.go_up(), "Up")
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkeypress(r_paddle.go_down, "Down")
 
-screen.onkey(r_paddle.go_up(), "w")
-screen.onkeypress(r_paddle.go_up, "w")
-screen.onkey(r_paddle.go_down, "s")
-screen.onkeypress(r_paddle.go_down, "s")
+screen.onkey(l_paddle.go_up(), "w")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
@@ -32,7 +34,20 @@ while game_is_on:
 
     #detect wall bounce
     if ball.ycor() > 290 or ball.ycor() < -290:
-        ball.bounce()
+        ball.bounce_y()
 
+    #detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 325 or ball.distance(l_paddle) < 50 and ball.xcor() < -325:
+        ball.bounce_x()
+
+    #when right paddle misses
+    if ball.xcor() > 400:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # when left paddle misses
+    if ball.xcor() < -400:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()

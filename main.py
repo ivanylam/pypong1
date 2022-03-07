c729882e6698e33c3e2c6e2cdb22c0a8a317c32c
Ivan.Lam <ivan.lam@fglsports.com>
2022-03-07 14:57:09 -0700

    paddle speed added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
-from  scoreboard import Scoreboard
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(800, 600)
@@ -28,7 +28,7 @@ screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
